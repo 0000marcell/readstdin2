bb4e3d79e70debb957324b9dba51f76c1b23ccf3
____marcell <0000marcell@gmail.com>
2019-06-12 20:25:08 +0000

    adding timeout when theres no stdin

--- readstdin.js
+++ readstdin.js
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 
+
 module.exports = function() {
   let stdin = process.stdin,
     inputChunks = [];
   return new Promise(function(resolve){
+    let timeout = () => {
+      stdin.destroy();
+    }
+
     stdin.resume();
     stdin.setEncoding('utf8');
 
     stdin.on('data', function (chunk) {
+      clearTimeout(timeout);
       inputChunks.push(chunk);
     });
 
     stdin.on('end', function () {
+      clearTimeout(timeout);
       resolve(inputChunks.join("")); 
     });  
+
+    setTimeout(timeout, 100)
   });
 }
