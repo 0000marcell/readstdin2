8981a21bb49a9165e96088f96557042409f702d7
____marcell <0000marcell@gmail.com>
2019-12-04 19:54:24 +0000

    removed timeout

--- readstdin.js
+++ readstdin.js
@@ -5,23 +5,15 @@ module.exports = function() {
   let stdin = process.stdin,
     inputChunks = [];
   return new Promise(function(resolve){
-    let timeout = () => {
-      stdin.destroy();
-    }
-
     stdin.resume();
     stdin.setEncoding('utf8');
 
     stdin.on('data', function (chunk) {
-      clearTimeout(timeout);
       inputChunks.push(chunk);
     });
 
     stdin.on('end', function () {
-      clearTimeout(timeout);
       resolve(inputChunks.join("")); 
     });  
-
-    setTimeout(timeout, 100)
   });
 }
